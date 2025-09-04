e8ccb3d88b7a26f9c7260a45b4f322e3e58dc772
Denize Mansoni <denizedtmc@gmail.com>
2025-09-03 21:46:02 -0300

    versão estável com front e back funcionando, tela de login ok e direcionamento para tela de cadastro

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import Cadastro from './pages/cadastro';
+import Dashboard from './pages/Dashboard';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Bem-vindo à Dashboard!</h1>
+      <p>Aqui você verá os dados cadastrados.</p>
+    </div>
+  );
+}
